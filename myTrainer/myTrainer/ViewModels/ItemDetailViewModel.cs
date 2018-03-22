d3194aac5bb54de97fc909cfcb2dfe18c408100b
Bauer <flo.bauer@online.de>
2018-03-22 18:18:30 +0100

    Projektdateien hinzufügen.

---
+++ myTrainer/myTrainer/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using myTrainer.Models;
+
+namespace myTrainer.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
